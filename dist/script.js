401bcab7c1f0462ffde46dfeab53cb4bd42a6ed6
Junyou Park <pjy008008@gmail.com>
2026-09-20 18:11:08 +0900

    Add milestone connectors and floating overlapping Bio bars (#10)

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-import {initBioTimeline} from './bio-timeline.js?v=af79a501e3';
+import {initBioTimeline} from './bio-timeline.js?v=bcffadfbbd';
 import {initMaterialInteractions} from './material-interactions.js?v=ce241d5078';
 import {createDepthReveal} from './depth-reveal.js';
 import {initAmbient} from './ambient.js?v=a4a6e13449';
