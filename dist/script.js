ac3595b45d081697dff4177ee70fc0aeadbd3ea9
Junyou Park <pjy008008@gmail.com>
2026-09-20 18:26:10 +0900

    Add CUVIC activities, leadership and club recognition to Bio

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-import {initBioTimeline} from './bio-timeline.js?v=bcffadfbbd';
+import {initBioTimeline} from './bio-timeline.js?v=b8aff8bbdd';
 import {initMaterialInteractions} from './material-interactions.js?v=ce241d5078';
 import {createDepthReveal} from './depth-reveal.js';
 import {initAmbient} from './ambient.js?v=a4a6e13449';
